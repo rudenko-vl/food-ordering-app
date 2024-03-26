c38b9c86be1214289c7e6cf8b66664a3be53e875
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2024-03-26 16:10:52 +0200

    made RegisterPage

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,26 +1,49 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
+import Link from "next/link";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [creatingUser, setCreatingUser] = useState(false);
   const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
   async function handleFormSubmit (ev) {
     ev.prventDefault();
     setCreatingUser(true);
-    await fetch('/api/register', {
+    setError(false);
+    setUserCreated(false);
+  
+    const response = await fetch('/api/register', {
       method: 'POST',
       body: JSON.stringify({email, password}),
       headers: {'Content-Type': 'application/json'},
     });
+    if (response.ok) {
+      setUserCreated(true);
+    } else {
+    setError(true);
+    }
     setCreatingUser(false);
   };
 
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      {userCreated && 
+      (<div className="my-4 text-center">
+          User created.<br/> 
+          Now you can{' '}
+          <Link className="underline" href={'/login'}>Login &raquo</Link>
+        </div>)}
+
+        {error && 
+      (<div className="my-4 text-center">
+          Error<br/> 
+          Please try again later
+        </div>)}
+
       <form action="" className="block mx-auto" onSubmit={handleFormSubmit}>
         <input
           type="email"
