a1379cefcdd01ca0016314b3043c4f3793821221
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2024-03-28 16:23:50 +0200

    add profile page

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,56 @@
+'use client';
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import {redirect} from "next/navigation";
+import Image from "next/image";
+
+export default function ProfilePage () {
+const session = useSession();
+const [userName, setUserName] = useState(session?.data?.user?.name || '');
+const {status} = session;
+ 
+async function handleProfileInfoUpdate (ev) {
+    ev.preventDefault();
+  
+    const response = await fetch("/api/profile", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: userName }),
+    });
+};
+
+if (status === 'loading') {
+return 'Loading...';
+}
+if (status === "unauthenticated") {
+    return redirect('/login');
+}
+
+const userImage = session.data.user.image;
+
+return (
+    <section className="mt-8">
+    <h1 className="text-center text-primary text-4xl mb-4 font-bold">
+        Profile
+    </h1>
+      <div className="max-w-md mx-auto">
+       <div className="flex gap-4 items-center">
+       <div>
+        <div className="p-2 rounded-lg relative">
+           <Image className="rounded-lg w-full h-full mb-1" src={userImage} width={250} height={250} alt={'avatar'}/>
+            <button type="button">Edit</button>
+        </div>
+        </div>
+        <form className="grow" onSubmit={handleProfileInfoUpdate}>
+            <input type="text" placeholder="First and last name"
+            value={userName} 
+            onChange={ev => setUserName(ev.target.value)}
+            />
+            <input type="email" disabled={true} value={session.data.user.email}/>
+            <button type="submit">Save</button>
+        </form>
+       </div>
+      </div>
+    </section>
+)
+};
