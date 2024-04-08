f49355d208bbd07eef18338ad722870717d88868
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2024-04-08 11:31:56 +0300

    changed profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -10,12 +10,26 @@ export default function ProfilePage() {
   const session = useSession();
   const [userName, setUserName] = useState("");
   const [image, setImage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
   const { status } = session;
 
   useEffect(() => {
     if (status === "authenticated") {
       setUserName(session.data.user.name);
       setImage(session.data.user.image);
+      fetch('/api/profile').then(response => {
+        response.json().then(data=> {
+          setPhone(data.phone);
+          setStreetAddress(data.streetAddress);
+          setPostalCode(data.postalCode);
+          setCity(data.city);
+          setCountry(data.country);
+        })
+      })
     }
   }, [session, status]);
 
@@ -25,7 +39,15 @@ export default function ProfilePage() {
       const response = await fetch("/api/profile", {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name: userName, image }),
+        body: JSON.stringify({ 
+          name: userName, 
+          image,
+          streetAddress,
+          phone,
+          postalCode,
+          city,
+          country,
+         }),
       });
       if (response.ok) resolve();
       else reject();
@@ -78,7 +100,7 @@ export default function ProfilePage() {
         Profile
       </h1>
       <div className="max-w-md mx-auto">
-        <div className="flex gap-4 items-center">
+        <div className="flex gap-4">
           <div>
             <div className="p-2 rounded-lg relative max-w-[120px]">
               {image && (
@@ -104,16 +126,57 @@ export default function ProfilePage() {
             </div>
           </div>
           <form className="grow" onSubmit={handleProfileInfoUpdate}>
+            <label>
+              First and last name
+            </label>
             <input
               type="text"
               placeholder="First and last name"
               value={userName}
               onChange={(ev) => setUserName(ev.target.value)}
             />
+            <label>Email</label>
             <input
               type="email"
               disabled={true}
               value={session.data.user.email}
+              placeholder="email"
+            />
+            <label>Phone</label>
+            <input type="tel"
+            placeholder="Phone number"
+            value={phone}
+            onChange={ev => setPhone(ev.target.value)}
+            />
+             <label>Street address</label>
+            <input type="text"
+            placeholder="Street address"
+            value={streetAddress}
+            onChange={ev => setStreetAddress(ev.target.value)}
+            />
+          <div className="flex gap-2">
+          <label>Postal code</label>
+         <div>
+         <input type="text"
+          placeholder="Postal code"
+          value={postalCode}
+          onChange={ev => setPostalCode(ev.target.value)}
+          />
+         </div>
+          <div>
+          <label>City</label>
+            <input type="text"
+            placeholder="City"
+            value={city}
+            onChange={ev => setCity(ev.target.value)}
+            />
+          </div>
+          </div>
+          <label>Country</label>
+            <input type="text"
+            placeholder="Country"
+            value={country}
+            onChange={ev => setCountry(ev.target.value)}
             />
             <button type="submit">Save</button>
           </form>
--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,12 @@ const UserSchema = new Schema({
     name: {type: String},
     email: { type: String, required: true, unique: true },
     password: {type: String },
-    image: {type: String}, 
+    image: {type: String},
+    phone: {type: String},
+    streetAddress: {type: String},
+    postalCode: {type: String},
+    city: {type: String},
+    country: {type: String},
   }, { timestamps: true });
 
 
